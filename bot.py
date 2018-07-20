0d27393ab930bd62259fc8f2d94d388fe5b560f7
rextyler9030 <38140305+rextyler9030@users.noreply.github.com>
2018-07-21 02:46:33 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+from bs4 import BeautifulSoup
+from threading import Thread
+def usernames():
+	users=[]
+	unames=open("usernames.txt",'r')
+	for uname in unames.readlines():
+			users.append(uname.strip('\n'))
+	return users
+        
+def passwords():
+	passwds=[]
+	passwords=open("passwords.txt",'r')
+	for password in passwords.readlines():
+			passwds.append(password.strip('\n'))
+	return passwds
+
+
+def Main(start,end):
+
+	for i in range(start,end):
+		t=Thread(target=enter(i))
+		t.start()
+def enter(i):
+		browser=webdriver.Chrome()
+		browser.get("https://jgi.i-on.in")
+
+		#Entering username
+		username=browser.find_element_by_id("cpusername")
+		uname=usernames()
+		username.send_keys(uname[i]) 
+
+
+	    # Entering password
+		password=browser.find_element_by_id("cppassword")
+		pwd=passwords()
+		password.send_keys(pwd[i])
+		
+		checkbox=browser.find_element_by_id("checkTerm")
+		checkbox.click()
+
+		password.submit()
+		browser.quit()
+
+
+	
+if (__name__=="__main__"):
+	start=int(input("Where to start?")) #because running all instances might freeze your system PS: browser.quit() doesnt'work
+	end=int(input("Where to end?"))
+	Main(start,end)
+
+
